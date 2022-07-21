bbe2563b63792d648d51869f2bcabe8f53a64329
Jayzeen <janithjayasinghe.nipuna@gmail.com>
2022-07-22 00:51:02 +0530

    Basic API requests finished

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,24 +5,82 @@ exports.getIndex = (req, res) => {
     header: "Available endpoints",
     user: [
       {
-        "Create User": "/create_user",
+        "Create User": "/user/create",
         "Get Users": "/users",
-        "Get User By ID": "/get_user/<id>",
-        "Update User By ID": "/update_user/<id>",
+        "Get User By Email": "/get_user/<Email>",
+        "Update User By Email": "/update_user/<Email>",
+        "Delete User By Email": "/user/delete"
       },
     ],
   });
 };
 
+// Creating a new user
+exports.createUser = (req, res) => {
+  const user = req.body;
 
-exports.getUsers = (req, res) => {
   userService
-    .getUsers()
+    .createUser(user)
     .then((users) => {
       res.status(200).json({ success: true, data: users });
     })
     .catch((err) => {
-      res.status(400).json({ success: false, error: "Get users failed" });
+      res.status(400).json({ success: false, error: "Creating a user failed" });
     });
 
 };
+
+// Get all users
+exports.getUsers = (req, res) => {
+  userService
+    .getUsers()
+    .then(users => {
+      res.status(200).json({ success: true, data: users });
+    })
+    .catch((err) => {
+      res.status(400).json({ success: false, error: "Getting list of Users failed" });
+    });
+
+};
+
+// Get user By email
+exports.getUserByEmail = (req, res) => {
+  email = req.params.email;
+
+  userService
+    .getUserByEmail(email)
+    .then(user => {
+      res.status(200).json({ success: true, data: user });
+    })
+    .catch((err) => {
+      res.status(400).json({ success: false, error: "Getting user by email failed" });
+    });
+};
+
+// Update user
+exports.updateUser = (req, res) => {
+  user = req.body;
+
+  userService
+    .updateUser(user)
+    .then(user => {
+      res.status(200).json({ success: true, data: user });
+    })
+    .catch((err) => {
+      res.status(400).json({ success: false, error: "Updating user by email failed" });
+    });
+};
+
+// Delete a user
+exports.deleteUser = (req, res) => {
+  email = req.body.email
+
+  userService
+    .deleteUser(email)
+    .then(user => {
+      res.status(200).json({ success: true, data: user });
+    })
+    .catch((err) => {
+      res.status(400).json({ success: false, error: "Deleting user by email failed" });
+    });
+}
--- services/user.service.js
+++ services/user.service.js
@@ -6,11 +6,55 @@ const uri = process.env.MONGODB_URI;
 const User = require('../models/User');
 
 
-exports.getUsers = async () => {
+//Creating a new user
+exports.createUser = async (user) => {
 
-  let user = new User({
-    username: "name",
-  }).save()
+  const newUser = await User.create(user);
+  return newUser
+}
+
+
+// Getting all users
+exports.getUsers = async () => {
+  const users = await User.find();
+
+  return users
+}
+
+// Get user by Email
+exports.getUserByEmail = async (email) => {
+  const query = {email: email};
+
+  const user = await User.find(query);
 
   return user
 }
+
+// Update user
+exports.updateUser = async (user) => {
+  const query = {email: user.email};
+  const update = user
+
+  const updatedUser = await User.findOneAndUpdate(query, update, {new: true});
+
+  return updatedUser;
+}
+
+// Delete a user
+exports.deleteUser = async (email) => {
+  const query = {email: email};
+
+  const response = await User.findOneAndDelete(query)
+
+}
+
+
+// {
+//   username: "test2",
+//   email: "test2@gmail.com",
+//   password: "test2pw",
+//   number: "0721234567",
+//   isRider: true,
+//   vehicle_type: "car",
+//   vehicle_no: "KF1234"
+// }
